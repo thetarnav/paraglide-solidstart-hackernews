3e9458685f509853c2e5ed87253153996c7ef576
Damian Tarnawski <gthetarnav@gmail.com>
2023-11-03 21:34:48 +0100

    Simplify paraglide adapter

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -3,11 +3,9 @@ import * as solid_web from 'solid-js/web'
 import * as messages from '@inlang/paraglide-js/hn/messages'
 import * as paraglide from '@inlang/paraglide-js/hn'
 
-const entries = <T extends object>(obj: T) => Object.entries(obj) as [keyof T, T[keyof T]][]
-
-export let m: typeof messages = messages
+export const m = messages
 export let languageTag: solid.Accessor<paraglide.AvailableLanguageTag> = paraglide.languageTag
-export const setLanguageTag = paraglide.setLanguageTag
+export let setLanguageTag = paraglide.setLanguageTag
 export const availableLanguageTags = paraglide.availableLanguageTags
 
 export const init = (): void => {
@@ -15,15 +13,7 @@ export const init = (): void => {
     if (solid_web.isServer) return
 
     const [_languageTag, _setLanguageTag] = solid.createSignal(paraglide.languageTag())
-    paraglide.onSetLanguageTag(_setLanguageTag)
-
+    paraglide.setLanguageTag(_languageTag)
+    setLanguageTag = _setLanguageTag
     languageTag = _languageTag
-
-    m = {...messages}
-    for (const [key, value] of entries(m)) {
-        m[key] = (a?: any) => {
-            languageTag() // track languageTag
-            return value(a)
-        }
-    }
 }
