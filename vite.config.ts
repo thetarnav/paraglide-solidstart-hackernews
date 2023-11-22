8d17581e9c57d3af39d4aa4e2ead1cd91b38dfb9
Damian Tarnawski <gthetarnav@gmail.com>
2023-11-22 23:20:23 +0100

    Add node adapter for local testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import * as vite from 'vite'
 import solid from 'solid-start/vite'
 import netlify from 'solid-start-netlify'
+// import nodeAdapter from 'solid-start-node'
 
 export default vite.defineConfig(({command}) => ({
     plugins: [
         solid({
             adapter: netlify({edge: true}),
+            // adapter: nodeAdapter(),
         }),
     ],
     optimizeDeps: {
         exclude: command === 'serve' ? ['@inlang/paraglide-js'] : [],
     },
+    // build: {minify: false},
 }))
